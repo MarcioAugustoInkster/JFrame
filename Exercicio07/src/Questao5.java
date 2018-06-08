0754b820ef1266328a6c26df45e1f8029ebad7e5
MarcioAugustoInkster <marcio_augusto_09@hotmail.com>
2018-06-07 22:50:17 -0300

    Lista 07
    Exercicios 1, 2, 3, 4, 5, 6, 7 e 8

--- Exercicio07/src/Questao5.java
+++ Exercicio07/src/Questao5.java
@@ -9,9 +9,30 @@ public class Questao5 {
 		
 		for (int i = 0; i < 4; i++) {
 			nomes.add(JOptionPane.showInputDialog(null, 
-				"Digite o " + (i + 1) + "º Nome:"));
+				"Informe o " + (i + 1) + "º Nome:"));
 		}
 		
+		String busca = JOptionPane.showInputDialog(null, 
+			"Informe um Nome para busca:");
 		
+		boolean encontrou = false;
+		
+		System.out.println("[ Lista de Nomes ]");
+		
+		for (int i = 0; i < nomes.size(); i++) {
+			System.out.println("[" + i + "] " + nomes.get(i));
+			
+			if (nomes.get(i).equalsIgnoreCase(busca)) {
+				encontrou = true;
+			}
+		}
+		
+		if (encontrou) {
+			JOptionPane.showMessageDialog(null, "Nome \"" + busca 
+				+ "\" existe na Lista.", "Informativo", JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			JOptionPane.showMessageDialog(null, "Nome \"" + busca 
+				+ "\" nao encontra-se na Lista.", "Aviso", JOptionPane.WARNING_MESSAGE);
+		}
 	}
 }
